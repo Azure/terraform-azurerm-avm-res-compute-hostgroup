53a73e8739680ed59caf2d95b669c5a1410c6ec7
chiancw <chiancw@gmail.com>
2024-04-03 20:17:32 +0800

    add dedicated hosts 1st draft

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-
+# Creates the dedicated host group
 resource "azurerm_dedicated_host_group" "example" {
   name                        = var.dedicated_host_group_name
   resource_group_name         = var.resource_group_name
@@ -7,4 +7,16 @@ resource "azurerm_dedicated_host_group" "example" {
   automatic_placement_enabled = var.automatic_placement_enabled
   zone                        = var.zone
   tags                        = var.tags
+}
+
+# Creates the dedicated host within the dedicated host group
+resource "azurerm_dedicated_host" "example" {
+  name                    = var.dedicated_host_name
+  location                = azurerm_resource_group.example.location
+  dedicated_host_group_id = azurerm_dedicated_host_group.example.id
+  sku_name                = var.dedicated_host_sku_name
+  platform_fault_domain   = var.platform_fault_domain
+  auto_replace_on_failure = var.auto_replace_on_failure
+  license_type            = var.license_type
+  tags                    = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -31,8 +31,37 @@ variable "dedicated_host_group_name" {
 variable "platform_fault_domain_count" {
   type        = number
   description = "The number of fault domains that the host group can span."
-  default     = 3
+  validation {
+    condition     = var.platform_fault_domain_count <= 5 && var.platform_fault_domain_count >= 1
+    error_message = "The platform fault domain count must be between 1 and 5."
+  }
 }
+
+# (Required) The name of the dedicated host
+variable "dedicated_host_name" {
+  type        = string
+  description = "The name of the dedicated host."
+}
+
+# (Required) The SKU of the dedicated host
+variable "dedicated_host_sku_name" {
+  type        = string
+  description = "The SKU of the dedicated host."
+
+  validation {
+    condition     = contains(local.valid_host_skus, var.dedicated_host_sku_name)
+    error_message = "sku must be 'apple', 'banana', or 'orange'"
+  }
+
+}
+
+# (Required) The platform fault domain of the dedicated host
+variable "platform_fault_domain" {
+  type        = number
+  description = "The platform fault domain of the dedicated host."
+}
+
+
 
 # (Optional) Would virtual machines or virtual machine scale sets be placed automatically on this Dedicated Host Group?
 variable "automatic_placement_enabled" {
@@ -53,4 +82,24 @@ variable "tags" {
   type        = map(any)
   description = "The map of tags to be applied to the resource"
   default     = {}
+}
+
+# (Optional) Specifies whether the host should be replaced automatically in case of a failure
+variable "auto_replace_on_failure" {
+  type        = bool
+  description = "Whether or not the host should be replaced automatically in case of a failure."
+  default     = true
+}
+
+# (Optional) Specifies the license type for the Dedicated Host
+variable "license_type" {
+  type        = string
+  description = "The license type for the Dedicated Host."
+  default     = "None"
+
+  # check if the provided value is None, Windows_Server_Hybrid or Windows_Server_Perpetual and return an error if it is not one of these values
+  validation {
+    condition     = var.license_type == "None" || var.license_type == "Windows_Server_Hybrid" || var.license_type == "Windows_Server_Perpetual"
+    error_message = "The license type must be one of None, Windows_Server_Hybrid or Windows_Server_Perpetual."
+  }
 }
